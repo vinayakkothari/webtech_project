c75d8c8d223a7e409ae05006584b72ef82e1a277
irka <kothari.vinayak2004@gmail.com>
2024-10-25 14:00:24 +0530

    login link

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Ecommerse-Site</title>
+        <title>E-commerce-Site</title>
         <link
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
             rel="stylesheet"
@@ -80,6 +80,9 @@
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="#">Contact Us</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#login.php">Log in</a>
                                 </li>
                             </ul>
                         </div>
@@ -370,6 +373,7 @@
             src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
             crossorigin="anonymous"
-        ></script>
+            <!-- Thank you Beyonce -->
+        </script>
     </body>
 </html>
