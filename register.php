ebb0f9fb40ad83f188475391f8791791082c43df
sibi361 <104310887+sibi361@users.noreply.github.com>
2024-11-01 14:12:09 +0530

    Add Register Page

--- register.php
+++ register.php
@@ -1,30 +1,30 @@
 <?php
 
+// Debug mode
+// error_reporting(E_ALL);
+// ini_set('display_errors', 'On');
+
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === "POST" && count($_POST)) {
     include "db.php";
 
     $username = mysqli_real_escape_string($conn, $_POST['username']);
-    $password = mysqli_real_escape_string($conn, hash("sha256", $_POST['password']));
 
-    $sql = "SELECT username, password, type FROM users WHERE username = '$username' AND password = '$password'";
+    $sql = "SELECT username type FROM users WHERE username LIKE '$username'";
     $result = mysqli_query($conn, $sql);
 
-    if (mysqli_num_rows($result) > 0) {
-        $row = mysqli_fetch_assoc($result);
+    if (mysqli_num_rows($result) === 0) {
+        echo "Registering...";
 
-        echo "Valid credentials... Logging in";
+        $password = mysqli_real_escape_string($conn, hash("sha256", $_POST['password']));
+        $type = "buyer";
 
-        $_SESSION["logged_in"] = true;
-        $_SESSION["username"] = $row["username"];
-        $_SESSION["type"] = $row['type'];
+        $sql = $conn->prepare("INSERT INTO users (username, password, type) VALUES (?, ?, ?)");
+        $sql->bind_param("sss", $username, $password, $type);
+        $sql->execute();
 
-        if ($_SESSION["type"] === "buyer") {
-            header("Location: /");
-        } else {
-            header("Location: /seller.php");
-        }
+        header("Location: /login.php");
     } else {
         echo '<!DOCTYPE html>
         <html lang="en">
@@ -33,12 +33,12 @@ if ($_SERVER['REQUEST_METHOD'] === "POST" && count($_POST)) {
                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>Smart Shop | Login</title>
-                <link rel="stylesheet" href="./assets/style.css" />
+                <link rel="stylesheet" href="assets/style.css" />
             </head>
             <body>
             <div class="container-login-error">
-                <p>Invalid credentials!</p>
-                <a href="/">Try again</a>
+                <p>Username has been taken</p>
+                <a href="/register.php">Try again</a>
             </div>
             </body>
         </html>';
@@ -50,22 +50,22 @@ if ($_SERVER['REQUEST_METHOD'] === "POST" && count($_POST)) {
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Smart Shop | Login</title>
+        <title>Smart Shop | Register</title>
         <link rel="stylesheet" href="assets/style.css" />
     </head>
     <body>
         <div class="container-login">
-            <h3>Login</h3>
+            <h3>Register</h3>
             <form method="POST">
                 <label for="name">Username:</label>
-                <input type="text" name="username" placeholder="buyer: sam   |   seller: ron" required /><br /><br />
+                <input type="text" name="username" required /><br /><br />
 
                 <label for="phone">Password:</label>
-                <input type="password" name="password" placeholder="  buyer: sam   |   seller: ron" required /><br /><br />
+                <input type="password" name="password" required /><br /><br />
 
                 <div style="display: flex; justify-content: space-between;">
                     <input type="submit" value="Submit" />
-                    <a href="/register.php">Register</a>
+                    <a href="/login.php">Login</a>
                 </div>
             </form>
         </div>
